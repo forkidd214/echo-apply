3caa59d538715f8f89a5e6ed513dc814fce90ace
zoufei <forkidd214@gmail.com>
2024-07-22 15:43:38 +0930

    fix: update block canvas

--- src/components/block-canvas/block-canvas.tsx
+++ src/components/block-canvas/block-canvas.tsx
@@ -10,15 +10,15 @@ export default function BlockCanvas({
   children,
 }: BlockCanvasProps) {
   return (
-    <div
-      className={cn(
-        'grid p-6',
-        isMobile
-          ? 'items-stretch justify-center [&>div]:aspect-[9/19.5]'
-          : 'items-center justify-stretch  [&>div]:aspect-[16/9]',
-      )}
-    >
-      <div className="border shadow-[0px_0px_6px_-1px_rgb(0,0,0,0.1),0_0px_4px_-2px_rgb(0,0,0,0.1)]">
+    <div className={cn('flex h-full w-full items-center justify-center p-6 ')}>
+      <div
+        className={cn(
+          'border shadow-[0px_0px_6px_-1px_rgb(0,0,0,0.1),0_0px_4px_-2px_rgb(0,0,0,0.1)]',
+          isMobile
+            ? 'aspect-[2/3] h-full max-h-[600px]'
+            : 'aspect-[16/9] w-full max-w-[1024px]',
+        )}
+      >
         {children}
       </div>
     </div>
