e9066cd410d5ae9bd083e4273135f3132884d6db
zoufei <forkidd214@gmail.com>
2025-06-06 21:55:41 +0930

    feat: enable form title renaming

--- src/components/form-title-input/form-title-input.tsx
+++ src/components/form-title-input/form-title-input.tsx
@@ -1,18 +1,35 @@
 'use client'
 
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, FocusEvent, useEffect, useState } from 'react'
 
 import { Input } from '@/components/ui/input'
 import { cn } from '@/utils/cn'
+import { useFormIdParams } from '@/utils/helpers'
+import { useFormRead, useFormUpdate } from '@/lib/use-form'
 
 type FormTitleInputProps = {}
 
 export default function FormTitleInput({}: FormTitleInputProps) {
-  const [value, setValue] = useState<string>('')
+  const formId = useFormIdParams()
+  const { data: formData } = useFormRead(formId)
+  const { mutate: updateForm } = useFormUpdate()
+
+  const [value, setValue] = useState<string>(formData?.name ?? '')
   const handleChange = (ev: ChangeEvent<HTMLInputElement>) => {
     setValue(ev.target.value)
   }
   const getInputWidth = (charNum: number) => `calc(1.5rem + ${charNum}ch)`
+
+  const handleBlur = (ev: FocusEvent<HTMLInputElement>) => {
+    updateForm({
+      id: formId,
+      name: value,
+    })
+  }
+
+  useEffect(() => {
+    setValue(formData?.name ?? '')
+  }, [formData])
 
   return (
     <Input
@@ -25,6 +42,7 @@ export default function FormTitleInput({}: FormTitleInputProps) {
       placeholder="Title"
       value={value}
       onChange={handleChange}
+      onBlur={handleBlur}
     />
   )
 }
